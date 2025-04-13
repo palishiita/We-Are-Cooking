88abd679bdf57685506da16d3a9e988c5473eb0c
Technosudo <technosudo@gmail.com>
2025-04-14 01:36:30 +0200

    main db with liquibase service, start scripts, proper routing and names

--- scripts/start-infra.sh
+++ scripts/start-infra.sh
@@ -1,4 +1,13 @@
+
+home="$(pwd)"/"$(dirname "$0")"..
+
+docker build -f "$home"/services/gateway/Dockerfile . -t wrc-gateway:latest
+
 docker compose \
---env-file "$( dirname "${BASH_SOURCE[0]}" )"/../infrastructure/.env \
--f "$( dirname "${BASH_SOURCE[0]}" )"/../infrastructure/keycloak/compose.dev.yaml \
+-f "$home"/infrastructure/keycloak/compose.dev.yaml \
+--env-file "$home"/infrastructure/.env \
 up -d
+
+
+sudo chown -R "$USER":"$USER" "$home"/../infrastructure/keycloak/authentication-DB-data
+sudo chmod -R 777 "$home"/../infrastructure/keycloak/authentication-DB-data
