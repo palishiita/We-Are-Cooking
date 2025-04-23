082c35b1d499ab0558bc714d03f5220cb7d30551
megabombus-rex <czechumennakompie@gmail.com>
2025-04-23 16:29:54 +0200

    Interfaces added and corrected.

---
+++ services/RecipesAPI/RecipesAPI/RecipesAPI/Services/Interfaces/IIngredientService.cs
@@ -0,0 +1,19 @@
+﻿using RecipesAPI.Model.Ingredients.Add;
+using RecipesAPI.Model.Ingredients.Get;
+
+namespace RecipesAPI.Services.Interfaces
+{
+    public interface IIngredientService
+    {
+        GetIngredientDTO GetIngredientById(Guid ingredientId);
+        IEnumerable<GetIngredientDTO> GetAllIngredients(int count, int page, bool orderByAsc, string sortBy);
+
+        IEnumerable<GetIngredientCategoryDTO> GetIngredientCategories(Guid ingredientId);
+
+        IEnumerable<GetIngredientCategoryDTO> GetAllIngredientCategories(int count, int page, bool orderByAsc, string sortBy);
+
+        Task<Guid> AddIngredient(AddIngredientDTO ingredientDTO);
+        Task<Guid> AddIngredientWithCategoriesByNames(AddIngredientWithCategoryNamesDTO ingredientDTO);
+        Task<Guid> AddIngredientWithCategoriesByIds(AddIngredientWithCategoryIdsDTO ingredientDTO);
+    }
+}
--- services/RecipesAPI/RecipesAPI/RecipesAPI/Services/Interfaces/IRecipeService.cs
+++ services/RecipesAPI/RecipesAPI/RecipesAPI/Services/Interfaces/IRecipeService.cs
@@ -1,22 +1,27 @@
-﻿using RecipesAPI.Model.Recipes.Get;
+﻿using RecipesAPI.Model.Recipes.Add;
+using RecipesAPI.Model.Recipes.Get;
 
 namespace RecipesAPI.Services.Interfaces
 {
     public interface IRecipeService
     {
         GetRecipeDTO GetRecipeById(Guid recipeId);
-        GetRecipeDTO[] GetRecipesByIds(Guid[] recipeIds, int count, int page, bool orderByAsc, string sortBy);
-        GetRecipeDTO[] GetAllRecipes(int count, int page, bool orderByAsc, string sortBy);
+        IEnumerable<GetRecipeDTO> GetRecipesByIds(IEnumerable<Guid> recipeIds, int count, int page, bool orderByAsc, string sortBy);
+        IEnumerable<GetRecipeDTO> GetAllRecipes(int count, int page, bool orderByAsc, string sortBy);
 
-        GetFullRecipeDTO[] GetFullRecipesByIds(Guid[] recipeIds, int count, int page, bool orderByAsc, string sortBy);
+        IEnumerable<GetFullRecipeDTO> GetFullRecipesByIds(IEnumerable<Guid> recipeIds, int count, int page, bool orderByAsc, string sortBy);
         GetFullRecipeDTO GetFullRecipeById(Guid recipeId);
 
         GetRecipeWithIngredientIdsDTO GetRecipeWithIngredientIds(Guid recipeId);
-        GetRecipeWithIngredientIdsDTO[] GetRecipesWithIngredientIdsByIds(Guid[] recipeIds, bool orderByAsc, string sortBy);
+        IEnumerable<GetRecipeWithIngredientIdsDTO> GetRecipesWithIngredientIdsByIds(IEnumerable<Guid> recipeIds, bool orderByAsc, string sortBy);
 
         GetRecipeWithIngredientsAndCategoriesDTO GetRecipeWithIngredientsAndCategories(Guid recipeId);
 
-        GetFullRecipeDTO[] GetAllFullRecipes(int count, int page, bool orderByAsc, string sortBy);
-        GetFullRecipeDTO[] GetFullRecipesByIngredientIds(Guid[] ingredientIds, int count, int page, bool orderByAsc, string sortBy);
+        IEnumerable<GetFullRecipeDTO> GetAllFullRecipes(int count, int page, bool orderByAsc, string sortBy);
+        IEnumerable<GetFullRecipeDTO> GetFullRecipesByIngredientIds(IEnumerable<Guid> ingredientIds, int count, int page, bool orderByAsc, string sortBy);
+
+        Task<Guid> CreateRecipe(AddRecipeDTO recipeDTO);
+        Task<Guid> CreateRecipeWithIngredientsByNames(AddRecipeWithIngredientNamesDTO recipeDTO);
+        Task<Guid> CreateRecipeWithIngredientsByIds(AddRecipeWithIngredientIdsDTO recipeDTO);
     }
 }
