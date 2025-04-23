326488785fc72802bb7cea0a31daca4a6d929823
megabombus-rex <czechumennakompie@gmail.com>
2025-04-23 16:30:05 +0200

    Added duplicate exceptions.

---
+++ services/RecipesAPI/RecipesAPI/RecipesAPI/Exceptions/DuplicateIngredientException.cs
@@ -0,0 +1,17 @@
+﻿namespace RecipesAPI.Exceptions
+{
+    public class DuplicateIngredientException : Exception
+    {
+        public DuplicateIngredientException()
+        {
+        }
+
+        public DuplicateIngredientException(string? message) : base(message)
+        {
+        }
+
+        public DuplicateIngredientException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ services/RecipesAPI/RecipesAPI/RecipesAPI/Exceptions/DuplicateRecipeException.cs
@@ -0,0 +1,17 @@
+﻿namespace RecipesAPI.Exceptions
+{
+    public class DuplicateRecipeException : Exception
+    {
+        public DuplicateRecipeException()
+        {
+        }
+
+        public DuplicateRecipeException(string? message) : base(message)
+        {
+        }
+
+        public DuplicateRecipeException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
