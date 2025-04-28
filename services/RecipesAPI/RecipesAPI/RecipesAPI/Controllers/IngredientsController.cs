82f854c5521e8f36aadb2a407d2b6f099950030e
JANC_PC\czech <czechumennakompie@gmail.com>
2025-04-28 11:04:43 +0200

    Added get methods for ingredients.

--- services/RecipesAPI/RecipesAPI/RecipesAPI/Controllers/IngredientsController.cs
+++ services/RecipesAPI/RecipesAPI/RecipesAPI/Controllers/IngredientsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using RecipesAPI.Exceptions.Duplicates;
 using RecipesAPI.Model.Ingredients.Add;
+using RecipesAPI.Model.Ingredients.Get;
 using RecipesAPI.Services.Interfaces;
 
 namespace RecipesAPI.Controllers
@@ -21,6 +22,67 @@ namespace RecipesAPI.Controllers
             _ingredientService = ingredientService;
         }
 
+        [HttpGet]
+        [Route("ingredient/{ingredientId:guid}")]
+        [ProducesResponseType(typeof(GetIngredientDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+        public IActionResult GetIngredientById([FromRoute] Guid ingredientId)
+        {
+            try
+            {
+                var ingredient = _ingredientService.GetIngredientById(ingredientId);
+                return Ok(ingredient);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("ingredients")]
+        [ProducesResponseType(typeof(IEnumerable<GetIngredientDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+        public IActionResult GetAllIngredients([FromQuery] int? count, [FromQuery] int? page, [FromQuery] bool? orderByAsc, [FromQuery] string? sortBy, [FromQuery] string? query)
+        {
+            count ??= 10;
+            page ??= 0;
+            orderByAsc ??= true;
+
+            sortBy = string.IsNullOrEmpty(sortBy) ? string.Empty : sortBy;
+            query = string.IsNullOrEmpty(query) ? string.Empty : query;
+
+            try
+            {
+                var ingredient = _ingredientService.GetAllIngredients(count.Value, page.Value, orderByAsc.Value, sortBy, query);
+                return Ok(ingredient);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("ingredient/{ingredientId:guid}/categories")]
+        [ProducesResponseType(typeof(GetIngredientWithCategoriesDTO), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+        public IActionResult GetFullIngredientById([FromRoute] Guid ingredientId)
+        {
+            try
+            {
+                var ingredient = _ingredientService.GetIngredientWithCategoriesById(ingredientId);
+                return Ok(ingredient);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return NotFound(ex.Message);
+            }
+        }
+
         [HttpPost]
         [Route("ingredient")]
         [ProducesResponseType(typeof(Guid), StatusCodes.Status201Created)]
@@ -38,7 +100,41 @@ namespace RecipesAPI.Controllers
             }
         }
 
+        [HttpPost]
+        [Route("ingredient/categories_ids")]
+        [ProducesResponseType(typeof(Guid), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> AddNewIngredientWithCategories([FromBody] AddIngredientWithCategoryIdsDTO ingredientDTO)
+        {
+            try
+            {
+                var id = await _ingredientService.AddIngredientWithCategoriesByIds(ingredientDTO);
+                return CreatedAtAction(nameof(AddNewIngredientWithCategories), id);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return BadRequest(ex.Message);
+            }
+        }
 
+        [HttpPost]
+        [Route("ingredient/categories_names")]
+        [ProducesResponseType(typeof(Guid), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> AddNewIngredientWithCategoriesByNames([FromBody] AddIngredientWithCategoryNamesDTO ingredientDTO)
+        {
+            try
+            {
+                var id = await _ingredientService.AddIngredientWithCategoriesByNames(ingredientDTO);
+                return CreatedAtAction(nameof(AddNewIngredientWithCategoriesByNames), id);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return BadRequest(ex.Message);
+            }
+        }
 
         [HttpPost]
         [Route("ingredient_category")]
